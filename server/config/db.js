bae3e4eee7cb1c6c981817a862f5292864296fd0
sangtosha <116915060+Vincentsang254@users.noreply.github.com>
2026-07-12 04:31:48 +0300

    Create db.js

---
+++ server/config/db.js
@@ -0,0 +1,28 @@
+const config = {
+	development: {
+		username: "sql8800339",
+		password: "W4SYzqlMsm",
+		database: "sql8800339",
+		host: "sql8.freesqldatabase.com",
+		dialect: "mysql",
+		port: 3306,
+	},
+	production: {
+		username: "sql8800339",
+		password: "W4SYzqlMsm",
+		database: "sql8800339",
+		host: "sql8.freesqldatabase.com",
+		dialect: "mysql",
+		port: 3306,    
+	},
+	test: {
+		username: "sql8800339",
+		password: "W4SYzqlMsm",
+		database: "sql8800339",
+		host: "sql8.freesqldatabase.com",
+		dialect: "mysql",
+		port: 3306,
+	}
+}
+
+module.exports = config;
