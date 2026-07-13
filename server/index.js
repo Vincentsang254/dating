e2208013e39096d8c995b37b5fbe1900ac51f21d
bkiplangatsang2001 <bkiplangatsang2001@gmail.com>
2026-07-13 07:58:10 +0000

    gggg

--- server/index.js
+++ server/index.js
@@ -44,7 +44,7 @@ app.use((err, req, res, next) => {
   if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../client/dist")));
 
-    app.get("*", (req, res) => {
+    app.use("*", (req, res) => {
       res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
     });
   }
