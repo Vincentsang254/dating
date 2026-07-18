5174eba7179f0b69e6866265b695c4cbefba1a02
jjeri8555 <jjeri8555@gmail.com>
2026-07-18 05:24:27 +0000

    hhhhh

--- client/src/components/customerView/customer-layout.jsx
+++ client/src/components/customerView/customer-layout.jsx
@@ -6,25 +6,25 @@ import CustomerSidebar from "./customer-sidebar.jsx";
 
 function UserLayout() {
   return (
-    <div className="flex flex-col h-screen">
+    <div className="min-h-screen flex flex-col">
       {/* Fixed Header */}
       <header className="sticky top-0 z-40 bg-white border-b border-gray-200">
         <UserHeader />
       </header>
-      
+
       {/* Main Content with Sidebar */}
-      <div className="flex flex-1 overflow-hidden">
+      <div className="flex flex-1 min-h-0">
         {/* Sidebar */}
         <CustomerSidebar />
-        
+
         {/* Main Content Area */}
         <main className="flex-1 overflow-y-auto">
-          <div className="w-full">
+          <div className="w-full min-h-full">
             <Outlet />
           </div>
         </main>
       </div>
-      
+
       {/* Footer */}
       <footer className="bg-gray-900 border-t border-gray-800">
         <UserFooter />
